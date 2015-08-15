fa443f57ccb8ed16cef4af79f584cea26090d70b
顾强 <guqiang@ycode.cn>
2015-08-15 10:12:32 +0800

    宏定义整理，常量以及不变的对象命名为AAA_BBB,判断为IsAaa

--- JCCommonUseManager/JCCommonUseManager/JCCommonUseManager/JCDEFINE.h
+++ JCCommonUseManager/JCCommonUseManager/JCCommonUseManager/JCDEFINE.h
@@ -9,15 +9,15 @@
 #import <UIKit/UIKit.h>
 // 各种尺寸
 // 屏幕宽度
-#define SCREENWIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
 // 屏幕高度
-#define SCREENHEIGHT [UIScreen mainScreen].bounds.size.height
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
 // 当前view,用于计算坐标
-#define CURRENTVIEW [UIApplication sharedApplication].keyWindow
+#define CURRENT_VIEW [UIApplication sharedApplication].keyWindow
 // 键盘高度
-#define KEYBOARDHEIGHT 216.f
+#define KEYBOARD_HEIGHT 216.f
 // 宏定义判断系统
-#define DeviceVersion [[[UIDevice currentDevice] systemVersion] integerValue]
+#define DEVICE_VERSION [[[UIDevice currentDevice] systemVersion] integerValue]
 // 是否是ipad
 #define IsPad (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 
